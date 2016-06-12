fd16251187d143e639d6ead9345d65a1c082d6d2
erfinw <erfinw@gmail.com>
2016-06-12 14:04:16 +0700

    Update testconn.php

--- testconn.php
+++ testconn.php
@@ -1,12 +1,12 @@
 <?php
 $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-
-$server = $url["host"];
-$username = $url["user"];
-$password = $url["pass"];
-$db = substr($url["path"], 1);
-
-$conn = new mysqli($server, $username, $password, $db);
+	
+	$server = $url["host"];
+	$username = $url["user"];
+	$password = $url["pass"];
+	$db = substr($url["path"], 1);
+	
+	$conn = mysqli_connect($server, $username, $password, $db);
 
 // Check connection
 if ($conn->connect_error) {
