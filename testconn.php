7c7b9ea0bda5b8068165f435fbbb72b1a7574c16
erfinw <erfinw@gmail.com>
2016-06-12 13:47:22 +0700

    Create testconn.php

---
+++ testconn.php
@@ -0,0 +1,16 @@
+<?php
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
+
+$conn = new mysqli($server, $username, $password, $db);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+?>
